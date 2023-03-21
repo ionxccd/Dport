76900ad2b8c6117f3ae3df2d3439865e8448a516
xccd <64030555+ionxccd@users.noreply.github.com>
2023-03-20 23:19:18 -0400

    Update dbController.js

--- src/controllers/dbController.js
+++ src/controllers/dbController.js
@@ -1,12 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-
-let url = "https://ptfbhpfztoubcujdxmwx.supabase.co";
-let anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0ZmJocGZ6dG91YmN1amR4bXd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY3NTAyNjUsImV4cCI6MTk5MjMyNjI2NX0.S6Ps8_BYheQVu2nO23mt1Snjc4_bxZKSyF1azEsLWdY";
-const supabase = createClient(url, anonKey)
-
-export const fun1 = () => {
-    console.log("kdjwd")
-}
+const supabase = createClient(import.meta.env.VITE_URL, import.meta.env.VITE_KEY);
 
 export const getLast6 = async () => {
     const { data, error } = await supabase
